d1512414684b3513064c322c70d62f8367b3ed83
luid <eng.limaluis@gmail.com>
2024-01-13 16:34:25 -0300

    moving my snake sssss

--- snakeGame.c
+++ snakeGame.c
@@ -1,10 +1,11 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <ncurses.h>
+#include <stdlib.h>
 
 #define cols 10
 #define rows 10
 
-
+int gameOver = 0;
 
 char matrix[cols * rows];
 
@@ -21,7 +22,8 @@ void fill_matrix(){
         for(x=0; x<cols; x++){
             if(x==0||y==0||x==cols-1||y==rows-1){
                 matrix[y*cols + x] = 'x';
-            }
+            }else
+                matrix[y*cols + x] = ' ';
         }
     }
 }
@@ -37,15 +39,41 @@ void print_matrix(){
         }
 }
 
+
+int snakeX = 5;
+int snakeY = 5;
+void draw_snake(){
+    matrix[snakeY*cols + snakeX] = '@';
+}
+
+void move_snake(int x, int y){
+    clear();
+    snakeX += x;
+    snakeY += y;
+}
+
+void read_keyboard(){
+    int ch = getch();
+    switch(ch){
+        case 'w': move_snake(0,-1); break;
+        case 's': move_snake(0,1); break;
+        case 'a': move_snake(-1,0); break;
+        case 'd': move_snake(1,0); break;
+    }
+}
+
 int main(int argc, char ** argv){
 
     initscr();
     init_matrix();
-    fill_matrix();
-    print_matrix();
-    refresh();
-    getch();
-    endwin();
+
+    while(1){
+        fill_matrix();
+        draw_snake();
+        print_matrix();
+        refresh();
+        read_keyboard();
+    }
     return 0;
 }
 
